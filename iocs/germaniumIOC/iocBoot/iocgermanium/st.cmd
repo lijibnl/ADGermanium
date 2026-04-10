a25114ef861a93252b9f07e2b6635c953d03b90f
Ji Li <solofrain@gmail.com>
2026-04-10 15:54:00 -0400

    Update st.cmd

--- iocs/germaniumIOC/iocBoot/iocgermanium/st.cmd
+++ iocs/germaniumIOC/iocBoot/iocgermanium/st.cmd
@@ -21,34 +21,32 @@ germaniumDetector_registerRecordDeviceDriver(pdbbase)
 germaniumConfig("$(PORT)", $(NELM), "$(ZYNQ_IP)", 2, 0, 50, 0)
 
 # Load detector PV records
-dbLoadRecords("$(ADGERMANIUM)/db/Germanium.template",
-              "P=$(PREFIX),R=,PORT=$(PORT),ADDR=0,NELM=$(NELM),MCA_NELM=$(MCA_NELM),TDC_NELM=$(TDC_NELM)")
+dbLoadRecords("$(ADGERMANIUM)/db/Germanium.template", "P=$(PREFIX),R=,PORT=$(PORT),ADDR=0,NELM=$(NELM),MCA_NELM=$(MCA_NELM),TDC_NELM=$(TDC_NELM)")
 
 ##=====================================================##
+
 # Load asyn records
-
-dbLoadRecords("$(ASYN)/db/asynRecord.db",
-              "P=$(PREFIX),R=asyn1,PORT=$(PORT),ADDR=0,OMAX=0,IMAX=0")
+dbLoadRecords("$(ASYN)/db/asynRecord.db", "P=$(PREFIX),R=asyn1,PORT=$(PORT),ADDR=0,OMAX=0,IMAX=0")
 
 ##=====================================================##
-# Load areaDetector plugins and records
 
+# Load areaDetector plugins and records
 < ad_plugins.cmd
 
 ##=====================================================##
-# Set path for auto-saving/restoring settings
 
+# Set path for auto-saving/restoring settings
 set_requestfile_path("$(ADGERMANIUM)/germaniumApp/Db")
 
 ##=====================================================##
-# Start the IOC
 
+# Start the IOC
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
 
 ##=====================================================##
-# Save/restore
 
+# Save/restore
 create_monitor_set("auto_settings.req", 30, "P=$(PREFIX)")
 
 ##=====================================================##
