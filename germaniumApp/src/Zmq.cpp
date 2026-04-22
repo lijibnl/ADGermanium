0ddcdd290a9f866fa8bb1a3a6ded2a882aca47e6
Ji Li <solofrain@gmail.com>
2026-04-22 09:56:12 -0400

    Create ZmqClient class

---
+++ germaniumApp/src/Zmq.cpp
@@ -0,0 +1,124 @@
+#include <cstring>
+#include <string>
+#include "Zmq.hpp"
+
+//===========================================================================//
+
+ZmqClient::ZmqClient( zmq::context_t&    ctx
+                    , const std::string& txEndpoint
+                    , const std::string& rxEndpoint
+                    )
+                    : ctx_       ( ctx        )
+                    , txEndpoint_( txEndpoint )
+                    , rxEndpoint_( rxEndpoint )
+{
+    try
+    {
+        initTxSocket();
+    }
+    catch (...)
+    {
+        return;
+    }
+
+    try
+    {
+        initRxSocket();
+    }
+    catch (...)
+    {
+        return;
+    }
+}
+
+//===========================================================================//
+
+void ZmqClient::initTxSocket()
+{
+    txSock_ = zmq::socket_t( ctx_, zmq::socket_type::push );
+
+
+    int linger = 0;
+    txSock_.set(zmq::sockopt::linger, linger);
+
+    int timeout = 1000;
+    txSock_.set(zmq::sockopt::sndtimeo, timeout);
+    
+    try
+    {
+        {
+            txSock_.connect( txEndpoint_ );            
+        }
+    }
+    catch(const zmq::error_t& e)
+    {
+        std::cerr << "ZMQ Tx socket connect error: " << e.what() << '\n';
+    }
+}
+
+//===========================================================================//
+
+void ZmqClient::initRxSocket()
+{
+    rxSock_ = zmq::socket_t( ctx_, zmq::socket_type::pull );
+
+    int linger = 0;
+    rxSock_.set(zmq::sockopt::linger, linger);
+    
+    int timeout = 1000;    
+    rxSock_.set(zmq::sockopt::rcvtimeo, timeout);
+        
+    try
+    {
+        {
+            rxSock_.connect( rxEndpoint_ );            
+        }
+    }
+    catch(const zmq::error_t& e)
+    {
+        std::cerr << "ZMQ Rx socket connect error: " << e.what() << '\n';
+    }
+}
+
+//===========================================================================//
+
+void ZmqClient::resetTxSocket()
+{
+
+    txSock_.close();
+   
+    initTxSocket();
+}
+
+//===========================================================================//
+
+bool ZmqClient::send_raw(const void* data, size_t size)
+{
+    zmq::message_t msg(size);
+    std::memcpy(msg.data(), data, size);
+
+    auto res = txSock_.send( msg, zmq::send_flags::none );
+    return res.has_value();
+}
+
+//===========================================================================//
+
+bool ZmqClient::recv_raw(void* data, size_t size)
+{
+    zmq::message_t msg;
+
+    if ( !rxSock_.recv( msg, zmq::recv_flags::none) )
+        return false;
+
+    if ( msg.size() != size )
+    {
+        std::cerr << "ZMQ Rx socket received message of unexpected size: " << msg.size() << " (expected " << size << ")\n";
+        return false;
+    }
+
+    std::memcpy(data, msg.data(), size);
+
+    return true;
+}
+
+//===========================================================================//
---
+++ germaniumApp/src/Zmq.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <type_traits>
+#include <atomic>
+#include <string>
+
+#include <zmq.hpp>
+
+template<typename T>
+concept ZmqMessage = std::is_trivially_copyable_v<T> &&
+                     std::is_standard_layout_v<T>;
+
+class ZmqClient
+{
+public:
+    ZmqClient( zmq::context_t&    ctx
+             , const std::string& txEndpoint
+             , const std::string& rxEndpoint
+             );
+
+    template<ZmqMessage T>
+    bool ZmqClient::tx( const T& msg )
+    {
+        return send_raw( &msg, sizeof(T) );
+    }
+
+    template<ZmqMessage T>
+    bool ZmqClient::rx( T& msg )
+    {
+        return recv_raw( &msg, sizeof(T) );
+    }
+
+    void resetTxSocket();
+
+private:
+
+    zmq::context_t&   ctx_;
+
+    const std::string txEndpoint_;
+    const std::string rxEndpoint_;
+
+    zmq::socket_t txSock_;
+    zmq::socket_t rxSock_;
+
+    std::atomic<bool> need_reset_{false};
+
+    void initTxSocket();
+    void initRxSocket();
+
+    bool send_raw(const void* data, size_t size);
+    bool recv_raw(void* data, size_t size);
+};
+
+enum class RecvStatus
+{
+    Ok,
+    Timeout,
+    SizeMismatch
+};
