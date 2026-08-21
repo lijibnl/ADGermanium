71cf45c3263f72dbfcc4eb7cd779101e9190dc98
Ji Li <liji@bnl.gov>
2026-08-21 17:12:59 -0400

    Update IOC build and run env

--- iocs/germaniumIOC/iocBoot/iocgermanium/unique.cmd
+++ iocs/germaniumIOC/iocBoot/iocgermanium/unique.cmd
@@ -10,4 +10,4 @@ epicsEnvSet("TDC_NELM",  "196608")
 epicsEnvSet("ZYNQ_MAN_IP",  "10.66.211.63")
 epicsEnvSet("ZYNQ_DATA_IP", "10.66.211.64")
 
-< /epics/common/xf27id1-det1-netsetup.cmd
+< /epics/common/xf27id1-det6-netsetup.cmd
